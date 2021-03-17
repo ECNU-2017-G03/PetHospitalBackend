1630a22508d0bc08a5789f74818262f48f073cf7
S_Lei <slei@berkeley.edu>
2021-03-17 14:46:08 +0800

    project skeleton
    refactor: add springboot code skeleton

---
+++ src/main/java/com/ecnu/g03/pethospital/controller/TestController.java
@@ -0,0 +1,28 @@
+package com.ecnu.g03.pethospital.controller;
+
+import com.ecnu.g03.pethospital.dto.response.TestResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author Shen Lei
+ * @Date 2021/3/17 14:19
+ */
+@RestController
+@CrossOrigin
+@PropertySource(value = "classpath:application.yml")
+public class TestController {
+
+    @Value("${storage.azure.account-name}")
+    private String azurename;
+
+    @GetMapping("/test")
+    public TestResponse test() {
+        TestResponse response = new TestResponse();
+        response.setMessage("hello world!" + azurename);
+        return response;
+    }
+}
---
+++ src/main/java/com/ecnu/g03/pethospital/dto/response/TestResponse.java
@@ -0,0 +1,14 @@
+package com.ecnu.g03.pethospital.dto.response;
+
+import lombok.Data;
+
+/**
+ * @Author Shen Lei
+ * @Date 2021/3/17 14:22
+ */
+@Data
+public class TestResponse {
+
+    String message;
+
+}
