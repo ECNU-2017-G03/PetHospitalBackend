8b4965038383923ae21552aeb07a2ce5ebb3de98
yueyue200830 <yueyue200830@163.com>
2021-03-23 20:13:59 +0800

    Read connection string from config

--- src/main/java/com/ecnu/g03/pethospital/controller/UserController.java
+++ src/main/java/com/ecnu/g03/pethospital/controller/UserController.java
@@ -1,28 +1,31 @@
 package com.ecnu.g03.pethospital.controller;
 
+import com.ecnu.g03.pethospital.dao.BaseTableDao;
 import com.ecnu.g03.pethospital.dto.response.TestResponse;
-import org.springframework.beans.factory.annotation.Value;
+import com.microsoft.azure.storage.table.CloudTableClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @Author Shen Lei
- * @Date 2021/3/17 14:19
+ * @author Jiayi Zhu
+ * @date 2021-03-22 10:27
  */
 @RestController
 @CrossOrigin
-@PropertySource(value = "classpath:application.yml")
-public class TestController {
+public class UserController {
 
-    @Value("${azure.storage.account-name}")
-    private String azurename;
+    @Autowired
+    BaseTableDao baseTableDao;
 
-    @GetMapping("/test")
+    @GetMapping("/user/login")
     public TestResponse test() {
         TestResponse response = new TestResponse();
-        response.setMessage("hello world!" + azurename);
+        CloudTableClient y = this.baseTableDao.getTableClient();
+        System.out.println(y);
+        response.setMessage("hello world!");
         return response;
     }
 }
--- src/main/java/com/ecnu/g03/pethospital/dao/BaseTableDao.java
+++ src/main/java/com/ecnu/g03/pethospital/dao/BaseTableDao.java
@@ -2,17 +2,19 @@ package com.ecnu.g03.pethospital.dao;
 
 import com.microsoft.azure.storage.CloudStorageAccount;
 import com.microsoft.azure.storage.table.CloudTableClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Juntao Peng
  * @date 2021/3/17 22:09
  */
+@Component
+@PropertySource(value = "classpath:application.yml")
 public class BaseTableDao {
-    private final String connectionString;
-
-    BaseTableDao() {
-        connectionString = System.getenv("PET_HOSPITAL_STORAGE_CONNECTION_STRING");
-    }
+    @Value("${azure.storage.connection-string}")
+    private String connectionString;
 
     public CloudTableClient getTableClient() {
         try {
